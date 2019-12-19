e6802f644540df06e2c21a760ddd1c705615eaf5
Scyzoryk <damianszefler96@gmail.com>
2019-12-19 23:21:44 +0100

    Zmiany stulu strony na cookies

--- js/jQuery.js
+++ js/jQuery.js
@@ -9,3 +9,35 @@ $(document).ready(function(){
             });
     });
 });
+
+$("#csschanger a[data-theme]").click(function() {
+    $("head link#theme").attr("href", $(this).data("theme"));
+    $.cookie("styleSheet", $(this).data("theme"), { expires : 365 });
+});
+
+$("#losuj").click(function() {
+    var themes = ["css/default.css","css/gray.css","css/green.css", "css/blue.css"];
+    var number = Math.floor(Math.random() * 4);
+    if($.cookie('number') == null || $.cookie('number') == undefined) {
+        $.cookie('number', number, {expires: 10});
+    }
+    if($.cookie('number') == number){
+            number++;
+        if(number>=3)
+        {
+            number=0;
+        }
+    }
+    $.cookie("number", number, {expires: 10});
+    $("head link#theme").attr("href", themes[number]);
+    $.cookie("styleSheet", themes[number], { expires : 365 });
+});
+
+function readCookieCSS(){
+    if($.cookie('styleSheet') === null || $.cookie('styleSheet')==undefined) {
+        /*Do nothing*/
+    }
+    else{
+        $("head link#theme").attr("href", $.cookie('styleSheet'));
+    }
+}
