ec418c52be5650d76706052c80d09666a8fcbbc2
Scyzoryk <damianszefler96@gmail.com>
2019-12-20 20:42:30 +0100

    wstrzykiwanui itp

--- js/jQuery.js
+++ js/jQuery.js
@@ -1,14 +1,18 @@
 $(document).ready(function(){
-    $('.nav-link').click(function(e){
-        e.preventDefault();
-        var content = $(this)
-        $("#dynamic").fadeOut(250, function(){
-            $("#dynamic").load(content.attr('href'));
-            });
-            $("#dynamic").fadeIn(250, function(){
-            });
-    });
+   $('#dynamic').load('subcontent/page0.html')
 });
+$('.nav-link').click(function(e){
+    e.preventDefault();
+    var content = $(this)
+    $("#dynamic").fadeOut(250, function(){
+        $("#dynamic").load(content.attr('href'));
+        });
+        $("#dynamic").fadeIn(250, function(){
+        });
+        return false;
+});
+
+
 
 $("#csschanger a[data-theme]").click(function() {
     $("head link#theme").attr("href", $(this).data("theme"));
